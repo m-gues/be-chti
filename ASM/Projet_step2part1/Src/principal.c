84bd1466042028ab1d8c56ae879b9b874f016125
m-gues <marianneguesss@gmail.com>
2020-03-30 08:21:18 +0200

    README mis à jour

--- ASM/Projet_step2part1/Src/principal.c
+++ ASM/Projet_step2part1/Src/principal.c
@@ -1,10 +1,14 @@
 int tab[64];
-int sommecarre(int);	
+int SommeCarre(int);	
+int max=0;
+int min=100;
 
 int main(void)
 {
 	for (int i=0;i<64;i++){
-		tab[i]=sommecarre(i);
+		tab[i]=SommeCarre(i);
+		if (tab[i]>max) max=tab[i];
+		if (tab[i]<min) min=tab[i];
 	}
 	
 while (1) {
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # be-chti
 Djihadi AHAMDY / Marianne GUES
+
+activité 1 : dossier signal-carre (fini)
+activité 2, partie 1 (sin2+cos2) : dossier Projet_step2part1 (en cours, ne compile pas encore)
