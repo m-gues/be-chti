4d98e91b4ad048659c061021e10702c7c6769ef3
m-gues <marianneguesss@gmail.com>
2020-03-30 09:19:25 +0200

    Partie 2-1 fonctionnelle

--- ASM/Projet_step2part1/Src/principal.c
+++ ASM/Projet_step2part1/Src/principal.c
@@ -1,12 +1,12 @@
 int tab[64];
-int SommeCarre(int);	
+int sommecarre(int);	
 int max=0;
 int min=100;
 
 int main(void)
 {
 	for (int i=0;i<64;i++){
-		tab[i]=SommeCarre(i);
+		tab[i]=sommecarre(i);
 		if (tab[i]>max) max=tab[i];
 		if (tab[i]<min) min=tab[i];
 	}
