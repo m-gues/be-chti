df082b2f40544f4b03723b71c9ff8cd0d60993ab
m-gues <marianneguesss@gmail.com>
2020-03-31 16:13:28 +0200

    partRe version 2

--- ASM/Projet_step2part2/partRe.s
+++ ASM/Projet_step2part2/partRe.s
@@ -1,34 +1,43 @@
 	thumb		
 	area  moncode, code, readonly
-
-	import TabCos
-	import TabSig
 		
 	export partRe
 		
 partRe proc
 	;on recup l'argument
+	; ici r0 contient k, r1 l'adresse de la table du signal, et r2 l'adresse de la table des cosinus. On les empile pour pouvoir réutiliser les registres.
+	push {r0}
+	push {r1}
+	push {r2}
 	
-	mov r12, #0x00
+	mov r12, #0x00 ;on met la variable d'itération à zéro
 	b loop
 	push {r12} ; on met un zéro tout en haut de la pile : c'est cette valeur qui sera incrémentée à la fin de chaque itération
 	
+	
 loop
 
-	;mettre l'@ de la valeur du signal et de Tabcos
-	ldr 	r2, =TabSig ;on met @ de TabSin
-	ldr 	r1, =TabCos ; on met l'@ de TabCos
+	; on dépile la valeur de retour et les arguments
+	pop {r0} ;valeur de retour
+	pop {r1} ;table des cosinus
+	pop {r2} ;table du signal
+	pop {r3} ;k
 	
-	mul r3, r12, r0 ;r3=ik
+	;on réempile immédiatement les arguments sans les modifier
+	push {r3} ;k
+	push {r2} ;signal
+	push {r1} ;cosinus
+
+	
+	mul r3, r12 ;r3=ik
 	
 	; on ajoute position et on recup la valeur
 	ldrsh 	r1, 	[r1,r3, lsl #0x01] ; r1 contient cos(ik)
 	ldrsh 	r2, 	[r2,r12, lsl #0x01] ; r2 contient x(i)
 	
-	mul r1, r2
-	pop {r3}
-	add r3, r1
-	push {r3}
+	mul r1, r2 ;r1=x(i)cos(ik)
+	add r0, r1 ;on ajoute r1 à la valeur de retour
+	push {r0} ;on empile la valeur de retour
 	
 	
 	;r12 contient la variable d'iteration
@@ -38,8 +47,6 @@ loop
 	b	fin
 	
 fin	
-	; On récupere le résultat
-	mov		 r0, r3
 	bx		lr
 	endp
 	end
